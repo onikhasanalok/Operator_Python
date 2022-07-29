356b91edcc1619491ef089276ec53bc5bb9dba9f
Onik Hasan <onikhasanalok123@gmail.com>
2022-07-29 21:20:08 +0600

    hi

--- Operator01.py
+++ Operator01.py
@@ -1,5 +1,18 @@
+#Operator
+
 a=25
 b=36
 c=a+b
 
 print(c)
+
+
+Price_of_first_Item=25/5
+Price_of_second_Item=25.5*2
+Price_of_third_Item=25.8-0.8
+Price_of_fourth_Item=25.9+0.1
+Price_of_fifth_Item=26/2-3
+
+Total=Price_of_fifth_Item+Price_of_second_Item+Price_of_third_Item+Price_of_first_Item+Price_of_fourth_Item
+
+print("Total Price of five Item: ",Total)
