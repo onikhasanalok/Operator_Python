cda01f48fe95a589438a886697d566fb3e1b97df
Onik Hasan <onikhasanalok123@gmail.com>
2022-07-29 21:49:22 +0600

    hi

--- Operator01.py
+++ Operator01.py
@@ -17,3 +17,16 @@ Price_of_fifth_Item=26/2-3
 Total=Price_of_fifth_Item+Price_of_second_Item+Price_of_third_Item+Price_of_first_Item+Price_of_fourth_Item
 
 print("Total Price of five Item: ",Total)
+
+#Type Casting (-,/,*.+)
+A=256
+B=5
+C=55
+D=A+B
+E=A-C
+F=B/C
+G=B*C
+print("Result of Plus Operator: ",D)
+print("Result of Minus Operator: ",E)
+print("Result of Division Operator: ",F)
+print("Result of Multiplication Operator: ",G)
